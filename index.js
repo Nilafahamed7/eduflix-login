bc8d89c2d71246401767150e3a644a02c4c577e9
nilafahamed7 <nilafiphone@gmail.com>
2025-07-25 14:22:44 +0530

    changes

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const helmet = require("helmet")
-app.use(helmet())
-app.use(cors())
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+require("dotenv").config()
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connexted"));
 
-var useremail = "welcome@gmail.com"
-var pass = "welcome007"
+const userdetails = mongoose.model("logindetails", {
+  useremail: String,
+  password: String,
+});
 
+// var useremail = "welcome@gmail.com"
+// var pass = "welcome007"
 
-app.get("/",function(req,res){
-    res.send("✅ Server is running!")
-})
+app.get("/", function (req, res) {
+  res.send("✅ Server is running!");
+});
 
-app.post("/login",function(req,res){
-    console.log(req.body.email)
-    if(useremail == req.body.email && pass == req.body.password){
-        res.send(true)
+app.post("/login", function (req, res) {
+  console.log(req.body.email);
+  userdetails.findOne({ useremail: req.body.email })
+  .then((user) => {
+    if (user && user.password === req.body.password) {
+      res.send(true);
+    } else {
+      res.send(false);
     }
-    else{
-        res.send(false)
-    }
-})
+  });
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,function(){
-    console.log(`server started on port ${PORT}...`)
-}) 
+app.listen(PORT, function () {
+  console.log(`server started on port ${PORT}...`);
+});
